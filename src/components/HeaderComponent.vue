a31f3615c3eb562e61e2423d27745702f0ba8a86
raffaelecapaldo <raffaelecpl@gmail.com>
2023-06-15 17:27:08 +0200

    Added navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,26 @@
 <template>
     <header>
+        <div class="nav-container">
+        <nav class="d-flex justify-content-between align-items-center text-uppercase py-3">
+            <div class="left">
+                <img class="logo" src="img/logo.png" alt="">
+            </div>
+            <div class="center d-flex gap-5">
+                <li><router-link :to="{name: 'home'}">Home</router-link></li>
+                <li><router-link :to="{name: 'characters'}">Characters</router-link></li>
+                <li><router-link to="">Rules</router-link></li>
+                <li><router-link to="">Team</router-link></li>
+                <li><router-link :to="{name: 'about'}">Contact Us</router-link></li>
+
+
+
+            </div>
+            <div class="right">
+                <li><router-link to="">Login</router-link></li>
+
+            </div>
+        </nav>
+    </div>
     </header>
 </template>
   
@@ -23,4 +44,29 @@ export default {
 }
 </script>
   
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    background-color: #202020;
+}
+.logo {
+    width: 180px;
+}
+
+.nav-container {
+    width: 95%;
+    margin:0 auto;
+
+    li {
+        list-style: none;
+        a {
+            text-decoration: none;
+            color:#F5E6C9;
+            font-size:24px;
+            font-weight: bold;
+        }
+    }
+}
+
+
+
+</style>
