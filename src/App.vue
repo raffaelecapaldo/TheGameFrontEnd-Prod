4ed45eef7bd4c65bef113b88e0ac96eef8d53f7b
Lip-7 <zhighofficial@gmail.com>
2023-06-15 11:55:16 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeaderComponent/>
+
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <HeaderComponent/>
+  <RouterView/>
+  <FooterComponent/>
+</template>
+
+<script>
+import FooterComponent from '../components/FooterComponent.vue';
+import HeaderComponent from '../components/HeaderComponent.vue';
+
+export default {
+  name: 'HomeView',
+  data() {
+      return {
+
+      }
+  },
+  methods: {
+
+  },
+  components: {
+    HeaderComponent,
+    FooterComponent
+},
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
