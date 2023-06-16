2069d793b0921205b664d02fe9276348035268e0
nicolozibra1 <nico.zibra@gmail.com>
2023-06-16 20:03:43 +0200

    fixed & refactoring navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,40 +1,40 @@
 <template>
     <header>
-        <div class="nav-container">
-        <nav class="d-flex justify-content-between align-items-center text-uppercase py-3">
-            <div class="left">
-                <img class="logo" src="/img/logo.png" alt="">
-            </div>
-            <div class="center d-flex gap-5">
-                <li><router-link :to="{name: 'home'}">Home</router-link></li>
-                <li><router-link :to="{name: 'characters'}">Characters</router-link></li>
-                <li><router-link to="">Rules</router-link></li>
-                <li><router-link to="">Team</router-link></li>
-                <li><router-link :to="{name: 'about'}">Contact Us</router-link></li>
-
-
-
+        <div class="nav-container h-100">
+            <nav class="d-flex justify-content-between align-items-center text-uppercase h-100">
+                <div class="box-logo">
+                    <img class="logo" src="/img/logo.png" alt="">
                 </div>
+                <!-- DESKTOP MENU -->
+                <div class="center h-100">
+                    <ul class="d-flex align-items-center gap-5 h-100">
+                        <li><router-link :to="{name: 'home'}">Home</router-link></li>
+                        <li><router-link :to="{name: 'characters'}">Characters</router-link></li>
+                        <li><router-link to="">Rules</router-link></li>
+                        <li><router-link to="">Team</router-link></li>
+                        <li><router-link :to="{name: 'about'}">Contact Us</router-link></li>
+                    </ul>
+                </div>
+                <!-- HAMBURGER MENU -->
                 <div @click="openhamb = !openhamb" class="hamb-button">
-                        <ul class="nav-mobile" :class="{'nav-mobile-open': openhamb}">
+                    <ul class="nav-mobile d-flex flex-column gap-3 pt-4" :class="{'nav-mobile-open': openhamb}">
                         <li><router-link :to="{ name: 'home' }">Home</router-link></li>
                         <li><router-link :to="{ name: 'characters' }">Characters</router-link></li>
                         <li><router-link to="">Rules</router-link></li>
                         <li><router-link to="">Team</router-link></li>
                         <li><router-link :to="{ name: 'about' }">Contact Us</router-link></li>
+                        <li class="mt-5 ms-5 text-center login-button"><a href="#">Login</a></li>
                     </ul>
-                    <svg viewBox="0 0 100 80" width="40" height="40">
-                        <rect width="100" height="20" rx="8"></rect>
-                        <rect y="30" width="100" height="20" rx="8"></rect>
-                        <rect y="60" width="100" height="20" rx="8"></rect>
-                    </svg>
+                    <i v-if="!this.openhamb" class="fa-solid fa-bars text-white fs-2 p-3"></i>
+                    <i v-if="this.openhamb" class="fa-solid fa-xmark text-white fs-1 p-3"></i> 
                 </div>
+                <!-- LOGIN -->
                 <div class="right">
                     <li><router-link to="">Login</router-link></li>
-
                 </div>
             </nav>
         </div>
+        <div class="overlay" :class="{'show': openhamb}"></div>
     </header>
 </template>
   
@@ -44,7 +44,6 @@ export default {
     data() {
         return {
             openhamb: false
-
         }
     },
     methods: {
@@ -63,41 +62,29 @@ export default {
 header {
     background-color: #202020;
     height: 70px;
+    padding: 0 20px;
+    .box-logo{
+        height: 100%;
+        width: 150px;
+        padding: 8px;
+        .logo {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    
 }
-
-.logo {
-    width: 180px;
-}
-
 .nav-container {
-    width: 95%;
-    margin: 0 auto;
-
     li {
         list-style: none;
-
+        border: 2px solid transparent;
         a {
             text-decoration: none;
-            font-size: 24px;
-            font-weight: bold;
+            font-size: 20px;
+            font-weight: 700;
             color:#F5E6C9;
         }
     }
-}
-
-.hamb-button {
-    display: none;
-    fill: #F5E6C9;
-    cursor:pointer;
-}
-
-
-
-@media screen and (max-width:1130px) {
-    .center {
-        display: none !important;
-    }
-
     .right {
         display: none;
     }
@@ -107,33 +94,56 @@ header {
     }
 
     .nav-mobile {
-        max-width: 200px;
-    position: absolute;
-    top: 7px;
-    right: 0%;
-    padding: 11px 20px;
-    top: 94px;
-    height: 0px;
-    background-color: #202020;
-    transition: 0.3s;
-    li {
-        a {
-            color:transparent;
-            transition:0s;
-        }
+        width: 70%;
+        position: absolute;
+        top: 69px;
+        right: 0;
+        z-index: 2;
+        opacity: 0;
+        transition: opacity 0.5s;
     }
-
-    
-    }
-
     .nav-mobile-open {
-        height: 200px!important;
-        li {
-            a {
-                color:#F5E6C9;
-            }
+        background-color: #202020;
+        height: calc(100vh - 70px) !important;
+        opacity: 1;
+    }
+    .login-button{
+        border: 1px solid #F8D67F;
+        width: 50%;
+    }
+}
+.center {
+    display: none !important;
+}
+.overlay{
+    height: 100vh;
+    width: 30%;
+    background-color: rgba(0, 0, 0, 0.397);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    opacity: 0;
+    transition: opacity 0.5s;
+}
+.overlay.show {
+  opacity: 1;
+}
+
+    @media screen and (min-width:1024px) {
+        header{
+            padding: 0 50px;
+        }
+        .hamb-button {
+            display: none !important;
+            fill: #F5E6C9;
+            cursor:pointer;
+        }
+        .right{
+            display: block !important;
+        }
+        .center{
+            display: block !important;
         }
     }
-
-}
 </style>
